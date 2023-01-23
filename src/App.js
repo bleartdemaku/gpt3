ab0d396c2b63358f7893b30f3eb36ad154e4a744
bleartdemaku <bd47165@ubt-uni.net>
2023-01-23 15:05:41 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Footer, Header, Blog, Features, Possibility, WhatGPT3 } from './containers';
-import { Cta, Brand, Navbar } from './components'
+import {
+  Footer,
+  Header,
+  Blog,
+  Features,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='gradient__bg'>
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
